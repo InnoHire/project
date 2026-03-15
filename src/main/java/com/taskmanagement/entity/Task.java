25083c241457f043032f0cfc3fbd9cfe51072025
srilekha <srilekhaearla1459@gmail.com>
2026-03-15 21:07:18 +0530

    user module implementation

---
+++ src/main/java/com/taskmanagement/entity/Task.java
@@ -0,0 +1,64 @@
+package com.taskmanagement.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private String technology;
+    private String status;
+
+    private Long managerId;
+    private Long userId;
+
+    private String solution;
+    private String testResult;
+
+    public Task() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getSolution() {
+        return solution;
+    }
+
+    public void setSolution(String solution) {
+        this.solution = solution;
+    }
+
+    public String getTestResult() {
+        return testResult;
+    }
+
+    public void setTestResult(String testResult) {
+        this.testResult = testResult;
+    }
+}
